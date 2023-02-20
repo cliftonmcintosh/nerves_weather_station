c5d29b5635c8e0260e52b917e4f7cc653919a9f9
Clifton McIntosh <cliff.mcintosh@gmail.com>
2023-02-20 11:34:26 -0600

    Add GenServer for VEML7700

--- veml7700/lib/veml7700.ex
+++ veml7700/lib/veml7700.ex
@@ -1,18 +1,68 @@
 defmodule VEML7700 do
   @moduledoc """
-  Documentation for `Veml7700`.
+  GenServer for the VEML7700 light sensor.
   """
+  use GenServer
 
-  @doc """
-  Hello world.
+  require Logger
 
-  ## Examples
+  alias VEML7700.Comm
+  alias VEML7700.Config
 
-      iex> Veml7700.hello()
-      :world
+  @impl true
+  def init(%{address: address, i2c_bus_name: bus_name} = args) do
+    i2c = Comm.open(bus_name)
 
-  """
-  def hello do
-    :world
+    config =
+      args
+      |> Map.take([:gain, :int_time, :shutdown, :interrupt])
+      |> Config.new()
+
+    Comm.write_config(config, i2c, address)
+    :timer.send_interval(1_000, :measure)
+
+    state = %{
+      i2c: i2c,
+      address: address,
+      config: config,
+      last_reading: :no_reading
+    }
+
+    {:ok, state}
+  end
+
+  def init(args) do
+    {bus_name, address} = Comm.discover()
+    transport = "bus: #{bus_name}, address: #{address}"
+
+    Logger.info("Starting VEML7700. Please specify an address and a bus.")
+    Logger.info("Starting on " <> transport)
+
+    args
+    |> Map.merge(%{address: address, i2c_bus_name: bus_name})
+    |> init()
+  end
+
+  @impl true
+  def handle_info(:measure, %{i2c: i2c, address: address, config: config} = state) do
+    reading = Comm.read(i2c, address, config)
+
+    updated_with_reading = %{state | last_reading: reading}
+    {:noreply, updated_with_reading}
+  end
+
+  @impl true
+  def handle_call(:get_measurement, _from, state) do
+    {:reply, state.last_reading, state}
+  end
+
+  @spec start_link(map()) :: GenServer.on_start()
+  def start_link(options \\ %{}) do
+    GenServer.start_link(__MODULE__, options, name: __MODULE__)
+  end
+
+  @spec get_measurement :: integer()
+  def get_measurement do
+    GenServer.call(__MODULE__, :get_measurement)
   end
 end
