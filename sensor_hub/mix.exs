36d3b8801bb10e8b5883e60030918581099599b8
Clifton McIntosh <cliff.mcintosh@gmail.com>
2023-03-05 16:28:20 -0600

    Add publisher to sensor_hub deps

--- sensor_hub/mix.exs
+++ sensor_hub/mix.exs
@@ -39,9 +39,10 @@ defmodule SensorHub.MixProject do
       # Dependencies for all targets except :host
       {:nerves_runtime, "~> 0.13.0", targets: @all_targets},
       {:nerves_pack, "~> 0.7.0", targets: @all_targets},
-      {:veml7700, path: "../veml7700", targets: @all_targets},
       {:sgp30, "~> 0.2.2", targets: @all_targets},
       {:bmp280, "~> 0.2.12", targets: @all_targets},
+      {:veml7700, path: "../veml7700", targets: @all_targets},
+      {:publisher, path: "../publisher", targets: @all_targets},
 
       # Dependencies for specific targets
       # NOTE: It's generally low risk and recommended to follow minor version
