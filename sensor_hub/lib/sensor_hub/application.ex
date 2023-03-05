cf974bf49367202615a22aca8b3f6892c58dd4ca
Clifton McIntosh <cliff.mcintosh@gmail.com>
2023-03-05 16:30:35 -0600

    Add WeatherTrackerClient and Publisher to SensorHub supervision tree

--- sensor_hub/lib/sensor_hub/application.ex
+++ sensor_hub/lib/sensor_hub/application.ex
@@ -5,6 +5,8 @@ defmodule SensorHub.Application do
 
   use Application
 
+  alias SensorHub.Sensor
+
   @impl true
   def start(_type, _args) do
     # See https://hexdocs.pm/elixir/Supervisor.html
@@ -29,11 +31,21 @@ defmodule SensorHub.Application do
     [
       {SGP30, []},
       {BMP280, [i2c_address: 0x77, name: BMP280]},
-      {VEML7700, %{}}
+      {VEML7700, %{}},
+      {Finch, name: WeatherTrackerClient},
+      {Publisher, %{sensors: sensors(), weather_tracker_url: weather_tracker_url()}}
     ]
   end
 
-  def target() do
+  defp target() do
     Application.get_env(:sensor_hub, :target)
+  end
+
+  defp sensors do
+    [Sensor.new(BMP280), Sensor.new(VEML7700), Sensor.new(SGP30)]
+  end
+
+  defp weather_tracker_url do
+    Application.get_env(:sensor_hub, :weather_tracker_url)
   end
 end
